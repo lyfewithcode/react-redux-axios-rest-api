a84b9143741c3a23537de24d6b963b44314ecec8
lyfewithcode <lyfewithcode@gmail.com>
2021-08-16 13:45:27 +0700

    useDispatch to dispatch action

--- src/containers/ProductListing.js
+++ src/containers/ProductListing.js
@@ -1,24 +1,27 @@
 import React, { useEffect } from 'react'
 import axios from 'axios'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import ProductComponent from './ProductComponent'
+import { setProducts } from '../redux/actions/productActions'
 
 const ProductListing = () => {
     const products = useSelector((state) => state)
+    const dispatch = useDispatch()
+
     const fetchProducts = async() => {
         const response = await axios
             .get('https://fakestoreapi.com/products')
             .catch((err) => {
                 console.log('Err', err)
             })
-        console.log(response.data)
+        dispatch(setProducts(response.data))
     }
 
     useEffect(() => {
         fetchProducts()
     }, [])
 
-    console.log(products)
+    console.log("Product: ", products)
     return (
         <div className='ui grid container'>
             <ProductComponent />
